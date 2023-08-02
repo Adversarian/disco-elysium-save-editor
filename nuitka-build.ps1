9ca1486594bac8f767414f2a9fc26555d05e9af5
Adversarian <a77physics@gmail.com>
2023-08-02 12:55:25 +0330

    Add nuitka build PS script.

---
+++ nuitka-build.ps1
@@ -0,0 +1,1 @@
+nuitka --onefile --windows-icon-from-ico="assets\save.png" --product-name="Disco Elysium Save Editor" --product-version="1.0.0" --file-description="Disco Elysium Save Editor" --output-filename="DESE" .\src\save_editor.py
