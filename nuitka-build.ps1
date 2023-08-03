df26e3b26198aa2b24ef1b32c9829cd3fc2f73cd
Adversarian <a77physics@gmail.com>
2023-08-03 11:30:31 +0330

    Split script in multiple lines for readability.

--- nuitka-build.ps1
+++ nuitka-build.ps1
@@ -1,1 +1,8 @@
-nuitka --onefile --windows-icon-from-ico="assets\save.png" --product-name="Disco Elysium Save Editor" --product-version="1.0.0" --file-description="Disco Elysium Save Editor" --output-filename="DESE" .\src\save_editor.py
+nuitka `
+--onefile `
+--windows-icon-from-ico="assets\save.png" `
+--product-name="Disco Elysium Save Editor" `
+--product-version="1.0.0" `
+--file-description="Disco Elysium Save Editor" `
+--output-filename="DESE" `
+.\src\save_editor.py
